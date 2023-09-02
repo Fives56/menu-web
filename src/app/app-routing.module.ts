eeada6d81825ced8e091441eb0ee350dd3e6a7c9
Victor Rivas <VictorRivas0230@gmail.com>
2023-09-02 19:28:53 -0400

    Add menu component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { OffersComponent } from './components/offers/offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/categories', pathMatch: 'full' },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'offers', component: OffersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
