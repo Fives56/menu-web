15cf4e9aa663dce0f9ec008a3becff409093159a
Victor Rivas <VictorRivas0230@gmail.com>
2023-09-03 11:09:57 -0400

    Add a route for a specific category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { OffersComponent } from './components/offers/offers.component';
+import { CategoryComponent } from './components/category/category.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/categories', pathMatch: 'full' },
   { path: 'categories', component: CategoriesComponent },
+  { path: 'categories/:id', component: CategoryComponent},
   { path: 'offers', component: OffersComponent },
+  
 ];
 
 @NgModule({
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { Category } from 'src/app/models/category.model';
 import { Food } from 'src/app/models/food.model';
+import { CategoryService } from 'src/app/services/category.service';
 import { FoodService } from 'src/app/services/food.service';
 
 @Component({
@@ -12,18 +15,63 @@ export class CategoryComponent implements OnInit {
   @Input() category!: Category;
 
   foods?: Food[];
+  id?: number;
+  order: string = 'name';
+  direction: string = 'asc';
+  limit: number = 2;
+  offset: number = 0;
+  count: number = 0;
 
-  constructor(private foodService: FoodService) {}
+  constructor(
+    private foodService: FoodService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) {}
 
+  /**
+   * Get the list of foods
+   */
   getFoods() {
-    this.foodService
-      .get(`?limit=2&category=${this.category.id}`)
-      .subscribe((data) => {
+    if (this.id && !this.category) {
+      this.categoryService.getOne(this.id).pipe(
+        switchMap((c) => {
+          this.category = c;
+          console.log(this.category);
+          this.limit = 10;
+          return this.foodService.get(this.getQuerys());
+        })
+      ).subscribe((data) => {
         this.foods = data.rows;
+        this.count = this.count;
       });
+    } else {
+      this.foodService.get(this.getQuerys()).subscribe((data) => {
+        this.foods = data.rows;
+        this.count = this.count;
+      });
+    }
+  }
+  
+
+  /**
+   * Get the querys for the petition http
+   * @returns String of the query
+   */
+  getQuerys() {
+    // const search: string = this.searchInput.nativeElement.value;
+    const params = new URLSearchParams();
+    // params.append('search', search);
+    params.append('order', this.order);
+    params.append('direction', this.direction);
+    params.append('limit', this.limit!.toString());
+    params.append('offset', this.offset!.toString());
+    params.append('category', this.category.id.toString());
+
+    return '?' + params.toString();
   }
 
   ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
     this.getFoods();
   }
 }
